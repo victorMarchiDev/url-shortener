529ea3c42dc1342cd1ad9e537e6f71634771dded
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-03-17 17:30:29 -0300

    Criando configuracao do CORS na aplicacao e preparando integracao com frontend

---
+++ src/main/java/victorMarchiDev/url_shortener/configuration/CorsConfig.java
@@ -0,0 +1,24 @@
+package victorMarchiDev.url_shortener.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Permite qualquer origem (ajuste conforme necessário)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permite todos os headers
+                        .allowCredentials(false); // Define se suporta cookies/sessões
+            }
+        };
+    }
+}
