ff18305ba3b96a51a6f37c15d80b9f8c18a399fe
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-03-17 15:52:17 -0300

    Segregando a logica da aplicacao e melhorando delegacao de responsabilidades da API criando a classe UrlService

--- src/main/java/victorMarchiDev/url_shortener/controller/UrlController.java
+++ src/main/java/victorMarchiDev/url_shortener/controller/UrlController.java
@@ -1,58 +1,43 @@
 package victorMarchiDev.url_shortener.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import victorMarchiDev.url_shortener.controller.dto.ShortenUrlRequest;
 import victorMarchiDev.url_shortener.controller.dto.ShortenUrlResponse;
-import victorMarchiDev.url_shortener.domain.Url;
-import victorMarchiDev.url_shortener.repository.UrlRepository;
-
+import victorMarchiDev.url_shortener.service.UrlService;
 
 import java.net.URI;
-import java.time.LocalDateTime;
 
 @RestController
 public class UrlController {
 
-    private final UrlRepository urlRepository;
+    private final UrlService urlService;
 
-    public UrlController(UrlRepository urlRepository) {
-        this.urlRepository = urlRepository;
+    public UrlController(UrlService urlService) {
+        this.urlService = urlService;
     }
 
     @PostMapping(value = "/shorten-url")
     public ResponseEntity<ShortenUrlResponse> shortenUrl(@RequestBody ShortenUrlRequest request,
                                                          HttpServletRequest servletRequest) {
-        String id;
-        do {
-            id = RandomStringUtils.randomAlphanumeric(5, 10);
-        } while (urlRepository.existsById(id));
 
-        urlRepository.save(new Url(id, request.url(), LocalDateTime.now().plusMinutes(1)));
-
+        String id = urlService.shortenUrl(request.url());
         var redirectUrl = servletRequest.getRequestURL().toString().replace("shorten-url", id);
 
         return ResponseEntity.ok(new ShortenUrlResponse(redirectUrl));
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Void> redirect(@PathVariable("id") String id) {
-
-        var url = urlRepository.findById(id);
-
-        if (url.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(URI.create(url.get().getFullUrl()));
-
-        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+    public ResponseEntity<Object> redirect(@PathVariable("id") String id) {
+        return urlService.getUrlById(id)
+                .map(url -> {
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.setLocation(URI.create(url.getFullUrl()));
+                    return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
-
-
 }
--- src/main/java/victorMarchiDev/url_shortener/service/UrlService.java
+++ src/main/java/victorMarchiDev/url_shortener/service/UrlService.java
@@ -1,4 +1,33 @@
 package victorMarchiDev.url_shortener.service;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.stereotype.Service;
+import victorMarchiDev.url_shortener.domain.Url;
+import victorMarchiDev.url_shortener.repository.UrlRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
 public class UrlService {
+
+    private final UrlRepository urlRepository;
+
+    public UrlService(UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
+
+    public String shortenUrl(String originalUrl) {
+        String id;
+        do {
+            id = RandomStringUtils.randomAlphanumeric(5, 10);
+        } while (urlRepository.existsById(id));
+
+        urlRepository.save(new Url(id, originalUrl, LocalDateTime.now().plusMinutes(1)));
+        return id;
+    }
+
+    public Optional<Url> getUrlById(String id) {
+        return urlRepository.findById(id);
+    }
 }
