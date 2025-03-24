95b9b19084e81e9bca2a67a277ac354a31bac892
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-03-24 14:19:54 -0300

    Correcoes relacionadas a uri de conexao com banco

--- src/main/java/victorMarchiDev/url_shortener/UrlShortenerApplication.java
+++ src/main/java/victorMarchiDev/url_shortener/UrlShortenerApplication.java
@@ -2,8 +2,10 @@ package victorMarchiDev.url_shortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "victorMarchiDev.url_shortener.repository")
 public class UrlShortenerApplication {
 
 	public static void main(String[] args) {
